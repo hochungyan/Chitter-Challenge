8c64ad4d006650650fed79c589f1173d27383020
Justin Ho <hochungyan@live.hk>
2022-12-19 16:14:19 +0000

    Header image added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,14 @@
-import logo from "../images/Chitter.mp4";
+import logo from "../images/Chitter.png";
 const Header = () => {
   return (
     <header>
  <div className="bg-light-yellow">
  <nav className="navbar navbar-expand-lg">
-  <video autoPlay loop muted width="150">
+  {/* <video autoPlay loop muted width="150">
     <source src = {logo} alt="Chitter" width="100"></source>
-  </video>
+  </video> */}
+<img src={logo} alt="Digital Futures" width="120" />
+
   <div className="text-white">
   <h3>Chitter App</h3>
   </div>
