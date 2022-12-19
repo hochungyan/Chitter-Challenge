b3a02d6a0b9df227c1d0a21b6ed791af6e7d60d5
Justin Ho <hochungyan@live.hk>
2022-12-19 16:42:25 +0000

    Profile Picture added after login in

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -5,6 +5,7 @@ import CreatePeep from "./Form/CreatePeep";
     <>
         <h5>Welcome to the homepage, {user.name}!</h5>
         <br />
+        <img height="32px" width="32px" src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-2.webp"/>
         <p>Your email address is: {user.email}</p>
         <button className="btn btn-outline-danger" onClick={() => setLoginUser({})}>Log out</button>
 <div className="container pb-3">
